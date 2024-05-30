4ad7cf1d3ca7f71228ba86def9f2793e4933d61d
edseab <ed.seabright@gmail.com>
2024-05-30 17:02:52 +0100

    minor fix

--- threeDimSimFiles/withoutMistakes/simulation commands.m
+++ threeDimSimFiles/withoutMistakes/simulation commands.m
@@ -20,5 +20,5 @@ gcc -o main main.o ContGroupAgent.o mt19937ar.o -lgnustep-base -lobjc -lgsl -lgs
 // Model with max number of steps of 20 (i.e. max possible difference between parent and mutant of 20*0.025 = 0.5)
 ./main 150001 5 2.0 100.0 0.5 100 0.0 0 999 999 0 0.02 20 16 3 1 1 0.0 "output_disagg_range.txt" "output_agg_range.txt" "output_diff_range.txt" 
 
-// Normal model
+// Model with Payback Parameter
 ./main 150001 5 2.0 100.0 0.5 100 0.0 0 999 999 0 0.02 1 16 3 1 0 10.0 "output_disagg.txt" "output_agg.txt" "output_diff.txt" 
